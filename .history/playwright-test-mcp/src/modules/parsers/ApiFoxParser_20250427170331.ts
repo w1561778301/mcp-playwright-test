5dd11ad825d5c15eb6262ec126a4e03a2afe85ba
owen.wang <owen.wang@huifu.com>
2025-04-28 16:14:01 +0800

    Remove .history folder and update .gitignore to exclude it

--- .history/playwright-test-mcp/src/modules/parsers/ApiFoxParser_20250427170331.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Version: 1.0.0
- * @Author: Owen.wang
- * @Date: 2025-04-27
- * @LastEditors: Owen.wang
- * @LastEditTime: 2025-04-27
- * @Description:
- */
--- .history/src/services/index_20250428105312.ts
+++
@@ -1,5 +0,0 @@
-export * from './git-service';
-export * from './playwright-service';
-export * from './test-generator-service';
-export * from './test-execution-service';
-export * from './api-testing-service';
